9d72a9f66610e4111f44542d3e2bc65dd7672897
SenyN <goumousenynat@gmail.com>
2024-01-15 18:17:54 +0100

    Resolution du projet calculatrice,avec la methode f-strings.

---
+++ calculatrice.py
@@ -0,0 +1,4 @@
+x =int(input("Entrez le premier nombre :"))
+y=int(input("Entrez le deuxième nombre :"))
+#print(f"le resultat de l'addition de {x} et {y} est egale à :{x+y} ")
+print("le resultat de l'addition de {} et {} est egale à : ".format(x,y))
