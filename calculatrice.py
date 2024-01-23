4340c0a0454ac689715dd90e43024863030eea74
SenyN <goumousenynat@gmail.com>
2024-01-24 00:28:14 +0100

    Affichage du resultat en utilisant la methode format.

--- calculatrice.py
+++ calculatrice.py
@@ -1,4 +1,11 @@
 x =int(input("Entrez le premier nombre :"))
 y=int(input("Entrez le deuxième nombre :"))
+# avec f-strings
 #print(f"le resultat de l'addition de {x} et {y} est egale à :{x+y} ")
-print("le resultat de l'addition de {} et {} est egale à : ".format(x,y))
+
+# Avec la methode format première methode
+print("Le resultat de l'addition de {} et {} est égal à : {}".format(x,y,x+y))
+
+
+# Avec la methode format deuxième methode
+#print("Le resultat de l'addition de {x} et {y} est egal à:{resultat}".format(x=x,y=y,resultat=x+y))
